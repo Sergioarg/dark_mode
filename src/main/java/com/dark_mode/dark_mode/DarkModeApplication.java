92ae8da9543d7d93d649c31c4d93d11984c406cc
sergioarg <sergio_ramos53@yahoo.com>
2023-03-01 20:32:14 -0500

    Create Project

---
+++ src/main/java/com/dark_mode/dark_mode/DarkModeApplication.java
@@ -0,0 +1,13 @@
+package com.dark_mode.dark_mode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarkModeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DarkModeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dark_mode/dark_mode/DarkModeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dark_mode.dark_mode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarkModeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
